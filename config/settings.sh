53a23cb6393f879bb0e7b4d5dcef6d8469b9938c
Abián <davidabian@wikimedia.es>
2019-02-22 13:41:54 +0000

    Increase `curl` timeout to 8 seconds
    Change-Id: Ic2fcb0330a8b9fe779681175370a25bf466c452a

--- config/settings.sh
+++ config/settings.sh
@@ -4,5 +4,5 @@
 
 declare -r LTSA_CURL_REFERER='https://tools.wmflabs.org/status/'
 declare -r LTSA_CURL_RETRIES=4
-declare -r LTSA_CURL_TIMEOUT_SECONDS=5
+declare -r LTSA_CURL_TIMEOUT_SECONDS=8
 declare -r LTSA_CURL_USER_AGENT='Availability checker of Wikimedia-related webpages'
