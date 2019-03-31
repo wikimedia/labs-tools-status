ad6c33c895493befb25e8e9ce3ecf7945606da11
Abián <davidabian@wikimedia.es>
2019-03-31 17:00:01 +0000

    Set Lydia Pintscher as a responsible for wikiba.se
    Change-Id: I99207ee13e8d129c60681fabcae5897319ff594c

--- config/owner_emails.sh
+++ config/owner_emails.sh
@@ -8,10 +8,10 @@ declare -Ar EMAILS=(
 	#  Add '' strings to prevent spam: 'use''r@mym''ail.com'
 	#
 	['blog.wikimedia.es']='platon''ides@gma''il.com'
-	['wikiba.se']='dav''idab''ian@wikim''edia.es'
+	['wikiba.se']='lydia.pi''ntscher@wikimedia.de'
 	['wikilov.es']='platon''ides@gma''il.com'
 	['wikimedia.es']='platon''ides@gma''il.com'
-	['www.wikiba.se']='dav''idab''ian@wikim''edia.es'
+	['www.wikiba.se']='lydia.pi''ntscher@wikimedia.de'
 	['www.wikilov.es']='platon''ides@gma''il.com'
 	['www.wikimedia.es']='platon''ides@gma''il.com'
 )
