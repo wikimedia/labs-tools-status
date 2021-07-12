65c47544968dccd2693559d098170895524feba3
Platonides <platonides@gmail.com>
2021-07-13 01:54:07 +0200

    Update WMES entries
    Change-Id: I68a537f9536d543023e173707290c4a1c74d7d03

--- config/owner_emails.sh
+++ config/owner_emails.sh
@@ -3,15 +3,25 @@
 # Email addresses of those responsible for some of the webpages.
 # An email will be sent to them when their webpages are unreachable.
 
+pam=  # Egg, bacon, sausage and $pam? No, thanks
+WMES=alertas$pam@wikimedia.es,'platon''ides@gma''il.com'
+
 declare -Ar EMAILS=(
 	#
 	#  Add '' strings to prevent spam: 'use''r@mym''ail.com'
 	#
-	['blog.wikimedia.es']='platon''ides@gma''il.com'
 	['wikiba.se']='lydia.pi''ntscher@wikimedia.de'
-	['wikilov.es']='platon''ides@gma''il.com'
-	['wikimedia.es']='platon''ides@gma''il.com'
 	['www.wikiba.se']='lydia.pi''ntscher@wikimedia.de'
-	['www.wikilov.es']='platon''ides@gma''il.com'
-	['www.wikimedia.es']='platon''ides@gma''il.com'
+
+	['wikilov.es']=$WMES
+	['wikimedia.es']=$WMES
+	['wiki.wikimedia.es']=$WMES
+	['servidor.wikimedia.es']=$WMES
+	['listas.wikimedia.es']=$WMES
+	['wikilm.es']=$WMES
+	['wikilovesearth.es']=$WMES
+	['www.wikilm.es']=$WMES
+	['www.wikilov.es']=$WMES
+	['www.wikilovesearth.es']=$WMES
+	['www.wikimedia.es']=$WMES
 )
